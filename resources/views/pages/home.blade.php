691d22078cfa1d55051b9bd48d96ccc21a5c79fa
ElisabettaMirti <belmirti@gmail.com>
2024-07-15 18:50:16 +0200

    inseriti fumetti nel main content in maniera dinamica

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,24 @@
 
 
 @section('main-content')
-    <h1>
-        Homepage
-    </h1>
+    <div class="main-content">
+        <div class="container">
+            <div class="current-titles">
+                <h2>current series</h2>
+            </div>
+            <section class="latest-comics">
+                @foreach ($latestComics as $comic)
+                    <article>
+                        <div class="img-cover">
+                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                        </div>
+                        <h4>{{ $comic['title'] }}</h4>
+                    </article>
+                @endforeach
+            </section>
+            <div class="button">
+                <p>load more</p>
+            </div>
+        </div>
+    </div>
 @endsection
