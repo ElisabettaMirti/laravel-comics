3bf54b8426b18fea157352b85e450ce588ba16cb
ElisabettaMirti <belmirti@gmail.com>
2024-07-15 18:43:35 +0200

    inseriti link dinamici nella navbar dell'header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,57 +6,16 @@
     <section class="navbar">
         <nav>
             <ul>
+                @foreach ($headerNav as $link)
                 <li >
                     <a href="#">
-                        characters
+                        {{ $link['name'] }}
                     </a>
                 </li>
-                <li >
-                    <a href="#">
-                        comics
-                    </a>
-                </li>
-                <li >
-                    <a href="#">
-                        movies
-                    </a>
-                </li>
-                <li >
-                    <a href="#">
-                        tv
-                    </a>
-                </li>
-                <li >
-                    <a href="#">
-                        games
-                    </a>
-                </li>
-                <li >
-                    <a href="#">
-                        collectibles
-                    </a>
-                </li>
-                <li >
-                    <a href="#">
-                        videos
-                    </a>
-                </li>
-                <li >
-                    <a href="#">
-                        fans
-                    </a>
-                </li>
-                <li >
-                    <a href="#">
-                        news
-                    </a>
-                </li>
-                <li >
-                    <a href="#">
-                        shop
-                    </a>
-                </li>
+                @endforeach
             </ul>
         </nav>
+
     </section>
+    <section class="jumbo"></section>
 </header>
