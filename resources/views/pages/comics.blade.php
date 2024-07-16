aff5f78fc57176060406a3f7fa81089aa1965f2d
ElisabettaMirti <belmirti@gmail.com>
2024-07-16 17:27:27 +0200

    creata nuova pagina e route "comics", e associata al link relativo nella nav dell'header

--- resources/views/pages/comics.blade.php
+++ resources/views/pages/comics.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.app')
 
-
 @section('main-content')
+    <div class="main-content">
+        <div class="container">
+            <div class="current-titles">
+                <h2>current series</h2>
+            </div>
+            <section class="latest-comics">
+                @foreach ($latestComics as $comic)
+                    <article>
+                        <div class="img-cover">
+                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                        </div>
+                        <h4>{{ $comic['title'] }}</h4>
+                    </article>
+                @endforeach
+            </section>
+            <div class="button">
+                <p>load more</p>
+            </div>
+        </div>
+    </div>
     <section class="merch-nav">
         <ul>
             <li>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,13 +6,56 @@
     <section class="navbar">
         <nav>
             <ul>
-                @foreach ($headerNav as $link)
                 <li >
-                    <a href="#">
-                        {{ $link['name'] }}
+                    <a href="/">
+                        Characters
                     </a>
                 </li>
-                @endforeach
+                <li >
+                    <a href="/comics">
+                        Comics
+                    </a>
+                </li>
+                <li >
+                    <a href="/">
+                        Movies
+                    </a>
+                </li>
+                <li >
+                    <a href="/">
+                        TV
+                    </a>
+                </li>
+                <li >
+                    <a href="/">
+                        Games
+                    </a>
+                </li>
+                <li >
+                    <a href="/">
+                        Collectibles
+                    </a>
+                </li>
+                <li >
+                    <a href="/">
+                        Videos
+                    </a>
+                </li>
+                <li >
+                    <a href="/">
+                        Fans
+                    </a>
+                </li>
+                <li >
+                    <a href="/">
+                        News
+                    </a>
+                </li>
+                <li >
+                    <a href="/">
+                        Shop
+                    </a>
+                </li>
             </ul>
         </nav>
 </header>
